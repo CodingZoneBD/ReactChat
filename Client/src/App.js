62eac8b49716977c15ff65b2e7aee16f0d87a581
Dalawer Hossain Juwel <juwel@bdtask.net>
2022-09-28 11:59:43 +0600

    React Coponents Create

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,17 @@
 import React from "react"
+import { BrowsweRouter as Router, route, Switch } from "react-router-dom"
+import Join from "./components/Join";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="App">
-      Hello ReactJS
+      <Router>
+        <Switch>
+          <Route path="/" compoent={Join} />
+          <Route path="/chat" compoent={Chat} />
+        </Switch>
+      </Router>
     </div>
   );
 }
