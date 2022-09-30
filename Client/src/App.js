4ad192eaffac3e13a09e67d7cd0af94819bed9a7
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2022-09-30 20:45:21 +0600

    Socket install for Client & Server

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
+// import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 
+import './App.css';
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
--- Client/src/pages/Chat.js
+++ Client/src/pages/Chat.js
@@ -1,4 +1,18 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string"
+import io from 'socket.io-client'
+let socket;
+
 const Chat = () => {
+    const { search } = useLocation();
+    const { name, room } = queryString.parse(search)
+
+    useEffect(() => {
+        socket = io("http://localhost:4000")
+    }, [])
+
+
     return <h1>Chat</h1>;
 };
 
